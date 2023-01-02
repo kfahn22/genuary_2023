42c20bec85dab3b514d6c64ef5caf3462d18563b
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-01-02 06:04:58 -0500

    initial commit

---
+++ 10_minutes/sketch.js
@@ -0,0 +1,15 @@
+let x,y,sc;
+
+function setup() {
+    createCanvas(400,00);
+}
+
+function draw() {
+    background(0);
+    stroke(255);
+    strokeWeight(3);
+    x = 40;
+    y = 40;
+    sc = 40;
+    circle(x, y, sc);
+}
